35e3aa62b608637678fff052098a9079322c6427
NathanC <nathas10.alves@gmail.com>
2026-03-25 08:14:43 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import './index.css'
+
+export default function App() {
+  const [leftPanelOpen, setLeftPanelOpen] = useState(true)
+  const [rightPanelOpen, setRightPanelOpen] = useState(true)
+
+  return(
+    <div className="app">
+      <aside className="sidebar">
+        <h1>Projeto Ferrovias A*</h1>
+        <p>Feerovias e rotas entre capitais</p>
+
+        <section className="panel">
+          <h2>Algoritmo</h2>
+          <select>
+            <option>A*</option>
+            <option>Malha Completa</option>
+            <option>Kruskal</option>
+            <option>AG</option>
+          </select>
+        </section>
+        
+        <section className="panel">
+          <h2>Parâmetros</h2>
+          <label>
+            Origem
+            <select>
+              <option>São Paulo</option>
+            </select>
+          </label>
+
+          <label>
+            Destino
+            <select>
+              <option>Rio de Janeiro</option>
+            </select>
+          </label>
+
+          <button>Run</button>
+        </section>
+
+        <section className="panel">
+          <h2>Sumário</h2>
+          <p>Distância Total: --</p>
+          <p>Custo Total: --</p>
+          <p>Transbordos: --</p>
+        </section>
+      </aside>
+
+      <main className="main">
+        <div className="map-area">
+          <p>Mapa</p>
+        </div>
+
+        <div className="details-panel">
+          <h2>Detalhes</h2>
+          <p>Amostragem de resultados detalhados</p>
+        </div>
+      </main>
+    </div>
+   )
+}
